b08484a7354669efd674fc79d45c0cd40c468ba3
Michael Thaler <michael.thaler2@gmail.com>
2026-03-21 09:55:30 +0100

    Updated utils.go.

--- utils.go
+++ utils.go
@@ -1,8 +1,16 @@
 package main
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
 
 func toInt(x float64) int {
 	n := math.Round(x)
 	return int(n)
+}
+
+func parseFloat(s string) (float64, error) {
+	f, err := strconv.ParseFloat(s, 64)
+	return f, err
 }
