6756e675c91f34aff4734c0dcaf20930f227f4de
Michael Thaler <michael.thaler2@gmail.com>
2026-03-21 08:26:12 +0100

    Updated heigt_weight.go.

--- heigt_weight.go
+++ heigt_weight.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"errors"
+)
+
 type HeightWeight struct {
 	height float64
 	weight float64
+}
+
+func (h HeightWeight) parse(text []string) (*HeightWeight, error) {
+	return nil, errors.New("could not parse height and weight")
 }
