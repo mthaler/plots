038299547870a2afa804a5f1e95955492a0fc66d
Michael Thaler <michael.thaler2@gmail.com>
2026-03-21 09:18:19 +0100

    Updated height_weight_test.go.

--- height_weight_test.go
+++ height_weight_test.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"regexp"
+	"log"
 	"testing"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func TestHelloName(t *testing.T) {
-	name := "Gladys"
-	want := regexp.MustCompile(`\b` + name + `\b`)
-	msg, err := Hello("Gladys")
+func TestHeightWeightParse(t *testing.T) {
+	hw1 := HeightWeight{}
+	hw2, err := hw1.parse([]string{"1", "2"})
+	if err != nil {
+		log.Fatal("Error while parsing height and weight")
+	}
 	if !want.MatchString(msg) || err != nil {
 		t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
